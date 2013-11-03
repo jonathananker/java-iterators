3bd486cd1f2f972845db40357ff88a47468a54d4
Shawn Shojaie <shawn.shojaie@gmail.com>
2013-11-03 02:23:47 -0800

    Finished part1.

--- CharSeq.java
+++ CharSeq.java
@@ -1,8 +1,27 @@
 
 public class CharSeq extends Seq{
 
+	String theString;
+
+
 	public CharSeq(String v){
+		theString = v;
 		return;
 	}
+
+
+	public String toString(){
+
+		String completeString = "{";
+
+		completeString += theString.length();
+		completeString += ": \"";
+		completeString += theString;
+		completeString += "\"}";
+
+
+		return completeString;
+	}
+
 
 }
--- IntSeq.java
+++ IntSeq.java
@@ -9,17 +9,22 @@ public class IntSeq	extends Seq {
 		numbersArray = numbers;
 		length = numbers.length;
 
-
-
 		return;
 	}
 
 	public String toString(){
+		String completeText = "[";
 
+		completeText += Integer.toString(length);
+		completeText += ":";
 
+		for(int i = 0; i < length; i++){
+			completeText += " " + numbersArray[i];
+		}
 
+		completeText += "]";
 
-		return Integer.toString(length);
+		return completeText;
 	}
 
 
